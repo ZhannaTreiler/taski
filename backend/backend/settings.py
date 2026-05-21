6833d505f7ca08f6cb59c0d22d028b05589fe09b
ZhannaTreiler <ali01.expresss@yandex.ru>
2026-05-22 01:00:33 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.170.58', '127.0.0.1', 'localhost','taskisdone.ddns.net']
+ALLOWED_HOSTS = ['158.160.170.58', '127.0.0.1', 'localhost','taskisdone1.ddns.net']
 
 
 # Application definition
